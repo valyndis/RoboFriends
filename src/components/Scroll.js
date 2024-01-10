dd2d2853863f2c435b14b63393d418a8618f4b23
Rui Custódio <ruifscustodio@gmail.com>
2024-01-10 19:25:35 +0000

    added ErrorBoundaries

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,7 +4,7 @@ const Scroll = (props) => {
     return (
         <div style={{overflowY: 'scroll', border: '1px solid black', height: '500px'}}>
             {props.children}
-    </div>
+        </div>
     );
 };
 
